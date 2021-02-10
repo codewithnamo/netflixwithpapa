3a55be7c004b0bc347ad7a4b175b60813ca0ae71
Harrison Kim <>
2021-02-10 10:15:28 +0800

    authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import ProfileScreen from "./screens/ProfileScreen";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // Logged In
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        // Logged out
+        dispatch(logout());
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
@@ -15,6 +39,9 @@ function App() {
           <LoginScreen />
         ) : (
           <Switch>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
             <Route exact path="/">
               <HomeScreen />
             </Route>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBskLdBR-Ac18qW-E2mkon8_3K--QgWSJE",
   authDomain: "netflix-clone-f4bdc.firebaseapp.com",
@@ -6,3 +8,13 @@ const firebaseConfig = {
   messagingSenderId: "928365092583",
   appId: "1:928365092583:web:05ede2843f726604d84c42",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// real time database
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export { auth };
+export default db;
